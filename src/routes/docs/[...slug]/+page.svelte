0102521d56a2e78f72c94b326ed53f67deb8f2bc
Prashant Kumar <prashantkry745@gmail.com>
2023-10-27 13:05:21 +0530

    Svelte Routing Nested Dynamic

--- src/routes/docs/[...slug]/+page.svelte
+++ src/routes/docs/[...slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { page } from "$app/stores";
     console.log($page.params.slug)
-    // for accessing routing part in code 
+    // for accessing routing part in code in 
 </script>
 <h2>Docs Page that can be accessed using any URL </h2>
